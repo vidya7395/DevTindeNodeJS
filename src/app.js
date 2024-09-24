a85e132f9ab5c6fba1785864c397eb63b501dfc6
Vidhya Satpute <vidhya7395@gmail.com>
2024-09-24 12:54:02 +0530

    exploring, get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.get("/user",(req,res)=>{
+    res.send({
+        "firstName":"Vidya"
+    })
+})
+app.post("/user",(req,res)=>{
+    res.send("save data to databse")
+})
 
-app.use("/test",(req,res)=>{
-    res.send("This is the response from a server, Hi Vidhya 🚀")
+app.delete("/user",(req,res)=>{
+    res.send("user data deleted from databse")
+})
+
+app.use("/",(req,res)=>{
+    res.send("404 resource not found 🚫 ")
 })
 app.listen("3000",()=>{
     console.log("Server is listening to 3000");
