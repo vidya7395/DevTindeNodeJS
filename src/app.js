2ee8c7d46ef26b2d35674d1f19462e70dba2e561
Vidhya Satpute <vidhya7395@gmail.com>
2024-10-01 11:58:48 +0530

    Schema validations

--- src/app.js
+++ src/app.js
@@ -5,18 +5,76 @@ const { connectDB } = require("./config/database")
 const Users = require("./model/user")
 
 app.use(express.json());
-app.post("/signup", async(req, res) => {    
+app.post("/signup", async (req, res) => {
     const user = new Users(req.body);
     try {
-     await user.save();
-     res.send("success")
-   } catch (error) {
-    res.status(400).send("Erro saving the user:" +error.message)
-   }
+        await user.save();
+        res.send("success")
+    } catch (error) {
+        res.status(400).send("Erro saving the user:" + error.message)
+    }
+
+
+});
+//get user from email
+
+app.get("/userEmail", async (req, res) => {
+    const userEmail = req.body.email;
+
+    try {
+        const user = await Users.findOne({
+            emailId: userEmail
+        });
+        if (!user) {
+            res.status(404).send("User not found")
+        }
+        else
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(400).send("Something went wrong" + error.message)
+    }
+
+
+});
+// get all the user from databse
+app.get("/feed", async (req, res) => {
+    try {
+        const user = await Users.find();
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(400).send("Something went wrong" + error.message)
+    }
 
 
 });
 
+//delete User
+app.delete("/user", async (req, res) => {
+   const  userId = req.body.userId;
+   try {
+        const user = await Users.findByIdAndDelete(userId);
+        res.send("User deleted successfully")
+         
+   } catch (error) {
+        console.log("error", error);
+        
+   }
+
+});
+app.patch("/user", async(req,res)=>{
+    const  userId = req.body.userId;
+    const data = req.body
+   try {
+        const user = await Users.findByIdAndUpdate(userId,data,{
+            runValidators: true
+        });
+        res.send("User updated successfully");
+         
+   } catch (error) {
+        console.log("error", error);
+        
+   }
+})
 connectDB()
     .then(() => {
         console.log("Database connection establish");
--- src/model/user.js
+++ src/model/user.js
@@ -2,24 +2,51 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     firstName:{
-        type: String
+        type: String,
+        required: true,
+        maxLenght:50,
+        minLenght: 4
     },
     lastName:{
         type: String
     },
     emailId:{
-        type: String
+        type: String,
+        lowercase: true,
+        trim: true,
+        required: true,
+        unique: true
     },
     password:{
         type: String
     },
     age:{
-        type: String
+        type: Number,
+        min: 18,
+        validate(value){
+            if(value<18){
+                throw new Error("The age should be less than 18")
+            }
+        },
+        
     },
     gender:{
-        type: String
+        type: String,
+        enum:["male","female","others"]
+    },
+    about:{
+        type:String,
+        defualt: " this is the about of the user!"  
+    },
+    photoUrl :{
+        type: String,
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfGUTrUMDG6phH5gx8RpKS0FGpmr1dpHsaZg&s"
+    },
+    skills:{
+        type: [String]
     }
-});
+
+}, {timestamps: true});
 
 const Users = mongoose.model("Users", userSchema);
 module.exports = Users;
