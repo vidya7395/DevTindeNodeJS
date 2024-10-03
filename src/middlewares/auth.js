ccb6004967153d42be97a8b2bf655826dcbea05f
Vidhya Satpute <vidhya7395@gmail.com>
2024-10-03 17:45:07 +0530

    created auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,30 +1,46 @@
- const adminAuth =  (req, res, next) => {
+const jwt = require("jsonwebtoken");
+const Users = require("../model/user");
+const adminAuth = (req, res, next) => {
     const requestToken = "xyzeee";
     let isAuthenticated = false;
     if (requestToken === "xyz") isAuthenticated = true;
     if (isAuthenticated) {
         next();
     }
-    else{
+    else {
         res.status(401).send("User is not authorised")
     }
 
 }
 
-const userAuth = (req,res,next)=>{
-    const {token:requestToken} = req.query;
-    let isAuthenticated = false;
-    if (requestToken === "xyz") isAuthenticated = true;
-    if (isAuthenticated) {
-        next();
-    }
-    else{
-        res.status(401).send("User is not authorised")
-    }
+const userAuth = async (req, res, next) => {
+   try {
+     const { token } = req.cookies;
+     if (!token) {
+         throw new Error("Inavlid Token, login again!");
+     }
+     const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
+     const { _id } = decodedObj;
+     if (!_id) {
+         throw new Error("Please login again!")
+     }
+     const user = await Users.findById(_id);
+     if(!user){
+        throw new Error("User not found!")
+     }
+     req.user = user;
+     next();
+   } catch (error) {
+    res.status(400).send("ERROR:" +error.message);
+   }
+    
 }
 
 
-module.exports ={
+
+
+
+module.exports = {
     adminAuth,
     userAuth
 }
