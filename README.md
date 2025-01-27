60bb9e516e67d07ac005b6abda88fd403190365b
Vidhya Satpute <vidhya7395@gmail.com>
2025-01-27 17:13:00 +0530

    Update README and user routes to add user feed API and improve data retrieval

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 ## //Review and response on the request
    
    POST - /request/review/:status/:requestId
+
+
+## userRouter
+- GET /user/feed - Gets you the profiles of other users on platform
+   - Get me data of other users
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
-const ConnectionRequestModel  = require("../model/connectionRequest");
+const ConnectionRequestModel = require("../model/connectionRequest");
+const Users = require("../model/user");
 //Get all the pending request for the logged in user
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     try {
-        const loggedInUser = req.user;        
+        const loggedInUser = req.user;
         const connectionRequests = await ConnectionRequestModel.find({
             toUserId: loggedInUser._id,
             status: "interested"
-        }).populate("fromUserId",USER_SAFE_DATA);
+        }).populate("fromUserId", USER_SAFE_DATA);
         // .populate("fromUserId",["firstName","lastName","photoUrl"]);
         // !This is the bad way (looping) we should use ref connection between two tables
         // const requestUsers = [];
@@ -27,9 +28,9 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
         //         message: "No requests found"
         //     })
         // }
-       
+
         res.json({
-            message:"Data fetched successfully",
+            message: "Data fetched successfully",
             data: connectionRequests
         })
     } catch (error) {
@@ -42,24 +43,62 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
         const connectionRequests = await ConnectionRequestModel.find({
-           $or: [
-            { fromUserId: loggedInUser._id,status: "accepted" },
-            { toUserId: loggedInUser._id,status: "accepted" }
-           ],
-            
-        }).populate("fromUserId",USER_SAFE_DATA).populate("toUserId",USER_SAFE_DATA); 
-        console.log(loggedInUser._id.toString());
-        
+            $or: [
+                { fromUserId: loggedInUser._id, status: "accepted" },
+                { toUserId: loggedInUser._id, status: "accepted" }
+            ],
+
+        }).populate("fromUserId", USER_SAFE_DATA).populate("toUserId", USER_SAFE_DATA);
         const data = connectionRequests.map((row) => row.fromUserId._id.equals(loggedInUser._id) ? row.toUserId : row.fromUserId);
         res.json({
-            message:"Data fetched successfully",
+            message: "Data fetched successfully",
             data: data
         });
 
     } catch (error) {
-        res.status(400).send({message:error.message})
+        res.status(400).send({ message: error.message })
     }
 
+});
+
+//Get data of all user : Feed API
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        console.log(loggedInUser);
+
+        //Remove the logged in user from the list
+        // let allUserExceptLoggedIn = allUser.filter((user) => user._id.toString() !== loggedInUser._id.toString());
+        // Remove the user who are already connected with the logged in user
+        const connectionRequests = await ConnectionRequestModel.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ],
+        }).select("fromUserId toUserId");
+        //Remove connection Request user from allUserExceptLoggedIn
+        // connectionRequests.forEach((request) => {
+        //     allUserExceptLoggedIn = allUserExceptLoggedIn.filter((user) => user._id.toString() !== request.fromUserId._id.toString() && user._id.toString() !== request.toUserId._id.toString());
+        // });
+        //Set data structure is like an array but it will not allow duplicate values , it will only contain unique values
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((request) => {
+            hideUsersFromFeed.add(request.fromUserId.toString());
+            hideUsersFromFeed.add(request.toUserId.toString());
+        })
+//$nin : not in this array , $ne means not equal to
+        const allUser = await Users.find({
+            $and: [{ _id: { $nin: Array.from(hideUsersFromFeed) } },
+            { _id: { $ne: loggedInUser._id } }],
+        }).select(USER_SAFE_DATA);;
+        res.json({
+            message: "Data fetched successfully",
+            data: allUser
+        });
+
+    } catch (error) {
+        res.status(400).send({ message: error.message })
+    }
 });
 
 module.exports = userRouter;
