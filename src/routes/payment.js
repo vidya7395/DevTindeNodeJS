e232ef5d4f65e697f31bcb711fcf575746821c51
Vidhya Satpute <vidhya7395@gmail.com>
2025-02-04 17:44:38 +0530

    fix: add logging for user verification in premium endpoint

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -88,6 +88,8 @@ paymentRouter.post("/payment/webhook", async (req, res) => {
 });
 paymentRouter.get("/premium/verify", async (req, res) => {
   const user = req.user;
+  console.log("req", req.user);
+  
   if(user.isPremium){
     return res.json({isPremium:true})
   }
